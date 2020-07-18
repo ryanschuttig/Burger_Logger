f3655f5cf52c26e8f9523960cb14943627e93f06
ryanschuttig <ryanschuttig@gmail.com>
2020-07-18 15:01:45 -0400

    Burgers everywhere

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Beyond');
+INSERT INTO burgers (name) VALUES ('Impossible');
+INSERT INTO burgers (name) VALUES ('Gardein');
+INSERT INTO burgers (name) VALUES ('Boca');
+INSERT INTO burgers (name) VALUES ('Veggie');
+INSERT INTO burgers (name) VALUES ('Black Bean');
