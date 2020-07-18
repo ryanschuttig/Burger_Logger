bceee3a41a4bed063eac696a97a319039a041da2
ryanschuttig <ryanschuttig@gmail.com>
2020-07-18 18:41:27 -0400

    Works

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,8 +1,8 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function () {
-  $(".change-burger").on("click", function (event) {
+  $(".change-devour").on("click", function (event) {
     var id = $(this).data("id");
-    var newDevour = $(this).data("newDevour");
+    var newDevour = $(this).data("newdevour");
 
     var newDevourState = {
       devour: newDevour
