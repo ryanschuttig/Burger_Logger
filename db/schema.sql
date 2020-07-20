64326b2aba2829c5502ec962b413265e9155fbb7
ryanschuttig <ryanschuttig@gmail.com>
2020-07-20 16:58:45 -0400

    Fix

--- db/schema.sql
+++ db/schema.sql
@@ -3,10 +3,13 @@ DROP DATABASE IF EXISTS burger_db;
 CREATE DATABASE burger_db;
 USE burger_db;
 
-CREATE TABLE burgers
-(
-	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
+DROP TABLE IF EXISTS burgers;
+
+CREATE TABLE burgers (
+	id INT AUTO_INCREMENT NOT NULL,
+	name VARCHAR(255) NOT NULL,
 	devour BOOLEAN DEFAULT false,
-	PRIMARY KEY (id)
+	description VARCHAR(255),
+	createdAt TIMESTAMP NOT NULL,
+	PRIMARY KEY(id)
 );
